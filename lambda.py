a45e95a0146b2decd45503c96e9e0488ea7dad6e
Bradley Diep <bdippin12@csu.fullerton.edu>
2022-04-27 23:34:09 -0700

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,21 +1,24 @@
-f = lambda x: 3*x+1
-print(f(2))
-
-def build_quad_formula(a,b,c):
-    return lambda x: a*x**2 + b*x + c
-
-g = build_quad_formula(3, 0, 1)(2)
-print(g)
-
-h = build_quad_formula(2, 3, -5)
-print(h(0))
-
-i = (lambda x:x**2) (9)
-print(i)
-
-j = (lambda y: (lambda x: x**2 + y**2))(2)(3)
-print(j)
-
 # How to make dfa with lambda calc:
 # take user input and convert each binary number as an element in a list?
+# If the number on the string is a 1, then the Turing Machine will simulate the DFA shown in Figure 6.3. 
+# If the number on the string is a 0, then the Turing Machine will simulate the DFA shown in Figure 6.4
 # based on certain positions a certain lambda function will trigger?
+# return 0 if string rejects
+# return 1 if string accepts
+# different lambda functions are different states and depending on their input, they'll transition into another state
+
+val0 = 0
+val1 = 1
+
+input_string = input("Enter your input string: ")
+print(input_string)
+
+new_string = [int(a) for a in str(input_string)]
+print(new_string)
+
+first = int(new_string[0])
+
+# prints first element in list if it's the only 1 of its kind
+# how to get elements of a list with lambda functions though?
+init_state = next(filter(lambda x: new_string[x] == 0, new_string), None)
+print(init_state)
