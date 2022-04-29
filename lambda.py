5295d211ee8d7c41bacfe71e5af50d6ff5976cb1
Bradley Diep <bdippin12@csu.fullerton.edu>
2022-04-28 19:23:47 -0700

    testing

--- lambda.py
+++ lambda.py
@@ -13,56 +13,62 @@
 # new_string = [int(a) for a in str(input_string)]
 # print(new_string)
 
-def state_q0(new_string):
-    # new_string = new_string[1:] # remove first character of string
+def state_q0(input_string):
+    new_string = input_string[1:] # remove first character of string
+    print(new_string) # prints list without the first element
     
-    init_state = (lambda x: state_q1(new_string) if (x == 0) else state_q5(new_string)) # (new_string[0])
+    # if list is empty return 0 bc rejected state
+    
+    # init_state = (lambda x: state_q1(new_string) if (x == 0) else state_q5(new_string)) # (new_string[0])
+#     # print(init_state) 
+#     print(init_state(new_string[0]))
+#     # return 0 # bc rejected state
+
+    init_state = (lambda x: "yes" if (x == 0) else "no") # (new_string[0])
     # print(init_state) 
-    print(init_state(new_string[0]))
-    # return 0 # bc rejected state
+    print(init_state(input_string[0]))
     
     
-# leads to Figure 6.4
-def state_q1(new_string): 
-    s1 = (lambda x: state_q3(new_string) if (x == 0) else state_q4(new_string))
-    print(s1(new_string[0]))
-    # return 1 # bc accepting state
+# # leads to Figure 6.4
+# def state_q1(new_string): 
+#     s1 = (lambda x: state_q3(new_string) if (x == 0) else state_q4(new_string))
+#     print(s1(new_string[0]))
+#     # return 1 # bc accepting state
     
-def state_q2(new_string):
-    s2 = (lambda x: state_q1(new_string) if (x == 0) else state_q4(new_string))
-    print(s2(new_string[0]))
-    # return 0 # bc rejected state
+# def state_q2(new_string):
+#     s2 = (lambda x: state_q1(new_string) if (x == 0) else state_q4(new_string))
+#     print(s2(new_string[0]))
+#     # return 0 # bc rejected state
     
-def state_q3(new_string):
-    s3 = (lambda x: state_q1(new_string) if (x == 0) else state_q2(new_string))
-    print(s3(new_string[0]))
-    # return 0 # bc rejected state
+# def state_q3(new_string):
+#     s3 = (lambda x: state_q1(new_string) if (x == 0) else state_q2(new_string))
+#     print(s3(new_string[0]))
+#     # return 0 # bc rejected state
     
-def state_q4(new_string):
-    s4 = (lambda x: state_q4(new_string) if (x == 0) else state_q4(new_string))
-    print(s4(new_string[0]))
-    # return 0 # bc rejected state
+# def state_q4(new_string):
+#     s4 = (lambda x: state_q4(new_string) if (x == 0) else state_q4(new_string))
+#     print(s4(new_string[0]))
+#     # return 0 # bc rejected state
     
     
-# leads to Figure 6.3
-def state_q5(new_string): 
-    s5 = (lambda x: state_q5(new_string) if (x == 0) else state_q6(new_string))
-    print(s5(new_string[0]))
-    # return 0 # bc rejected state
+# # leads to Figure 6.3
+# def state_q5(new_string): 
+#     s5 = (lambda x: state_q5(new_string) if (x == 0) else state_q6(new_string))
+#     print(s5(new_string[0]))
+#     # return 0 # bc rejected state
     
-def state_q6(new_string): 
-    s6 = (lambda x: state_q6(new_string) if (x == 0) else state_q5(new_string))
-    print(s6(new_string[0]))
+# def state_q6(new_string): 
+#     s6 = (lambda x: state_q6(new_string) if (x == 0) else state_q5(new_string))
+#     print(s6(new_string[0]))
     # return 1 # bc accepting state
 
 
 def main():
     input_string = input("Enter your input string: ")
-    print(input_string)
 
-    new_string = [int(a) for a in str(input_string)]
-    print(new_string)
-    state_q0(new_string)
+    input_string = [int(a) for a in str(input_string)]
+    print(input_string)
+    state_q0(input_string)
 
 
 if __name__ == '__main__':
