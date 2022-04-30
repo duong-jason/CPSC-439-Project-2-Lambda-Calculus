dac8162e172423d5bd0faad3d2758592ab8a8643
Jason Duong <reddkingdom@csu.fullerton.edu>
2022-04-29 18:27:33 -0700

    Added Shebang.

--- lambda.py
+++ lambda.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # CPSC 439-01 (Spring 2022) Project 2: Lambda Calculus
 
 # Group Members: 
@@ -15,10 +16,10 @@ def state_q0(input_string):
     init_state = (lambda x: state_q1(new_string) if (x == 0) else state_q5(new_string)) # (new_string[0])
     # print(init_state) 
     print(init_state(input_string[0]))
-    
-    
+
+
 # # leads to Figure 6.4
-def state_q1(input_string): 
+def state_q1(input_string):
     if(len(input_string) == 0):
         sys.exit('1') # 1 shows that it's an accepted string
         # return 1 
@@ -26,8 +27,8 @@ def state_q1(input_string):
         new_string = input_string[1:]
         s1 = (lambda x: state_q3(new_string) if (x == 0) else state_q4(new_string))
         print(s1(input_string[0]))
-    
-    
+
+
 def state_q2(input_string):
     if (len(input_string) == 0):
         # return 0 
@@ -36,8 +37,8 @@ def state_q2(input_string):
         new_string = input_string[1:]
         s2 = (lambda x: state_q1(new_string) if (x == 0) else state_q4(new_string))
         print(s2(input_string[0]))
-    
-    
+
+
 def state_q3(input_string):
     if (len(input_string) == 0):
         # return 0 
@@ -46,7 +47,7 @@ def state_q3(input_string):
         new_string = input_string[1:]
         s3 = (lambda x: state_q1(new_string) if (x == 0) else state_q2(new_string))
         print(s3(input_string[0]))
-            
+
 
 def state_q4(input_string):
     if (len(input_string) == 0):
@@ -56,25 +57,25 @@ def state_q4(input_string):
         new_string = input_string[1:]
         s4 = (lambda x: state_q4(new_string) if (x == 0) else state_q4(new_string))
         print(s4(input_string[0]))
-    
-    
+
+
 # # leads to Figure 6.3
-def state_q5(input_string): 
+def state_q5(input_string):
     if (len(input_string) == 0):
         # return 0 
         sys.exit('0') # 0 shows that it's a rejected string
     else:
-        new_string = input_string[1:] 
+        new_string = input_string[1:]
         s5 = (lambda x: state_q5(new_string) if (x == 0) else state_q6(new_string))
         print(s5(input_string[0]))
-    
-    
-def state_q6(input_string): 
+
+
+def state_q6(input_string):
     if (len(input_string) == 0):
         # return 1 
         sys.exit('1') # 1 shows that it's an accepted string
     else:
-        new_string = input_string[1:] 
+        new_string = input_string[1:]
         s6 = (lambda x: state_q6(new_string) if (x == 0) else state_q5(new_string))
         print(s6(input_string[0]))
 
@@ -85,10 +86,6 @@ def main():
     data = open("testfiles/" + value, "r")
     input_string = data.read()
 
-  
-
-
-    
     if ("0" in input_string or "1" in input_string):
         input_string = [int(a) for a in str(input_string)]
         state_q0(input_string)
@@ -97,4 +94,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()    
+    main()
