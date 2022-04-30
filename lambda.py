9f3cf0f6f67b279483cc1edbd6bd5db0bdba58e3
Jason Duong <reddkingdom@csu.fullerton.edu>
2022-04-29 18:31:50 -0700

    Cleaned Code a bit.

--- lambda.py
+++ lambda.py
@@ -7,7 +7,9 @@
 # Jason Duong
 # Omid Nikjoo
 
+
 import sys
+
 
 def state_q0(input_string):
     new_string = input_string[1:] # remove first character of string
@@ -20,7 +22,7 @@ def state_q0(input_string):
 
 # # leads to Figure 6.4
 def state_q1(input_string):
-    if(len(input_string) == 0):
+    if not len(input_string):
         sys.exit('1') # 1 shows that it's an accepted string
         # return 1 
     else:
@@ -30,7 +32,7 @@ def state_q1(input_string):
 
 
 def state_q2(input_string):
-    if (len(input_string) == 0):
+    if not len(input_string):
         # return 0 
         sys.exit('0') # 0 shows that it's a rejected string
     else:
@@ -40,7 +42,7 @@ def state_q2(input_string):
 
 
 def state_q3(input_string):
-    if (len(input_string) == 0):
+    if not len(input_string):
         # return 0 
         sys.exit('0') # 0 shows that it's a rejected string
     else:
@@ -50,7 +52,7 @@ def state_q3(input_string):
 
 
 def state_q4(input_string):
-    if (len(input_string) == 0):
+    if not len(input_string):
         # return 0 
         sys.exit('0') # 0 shows that it's a rejected string
     else:
@@ -61,7 +63,7 @@ def state_q4(input_string):
 
 # # leads to Figure 6.3
 def state_q5(input_string):
-    if (len(input_string) == 0):
+    if not len(input_string):
         # return 0 
         sys.exit('0') # 0 shows that it's a rejected string
     else:
@@ -71,7 +73,7 @@ def state_q5(input_string):
 
 
 def state_q6(input_string):
-    if (len(input_string) == 0):
+    if not len(input_string):
         # return 1 
         sys.exit('1') # 1 shows that it's an accepted string
     else:
@@ -82,12 +84,12 @@ def state_q6(input_string):
 
 def main():
     value = input("please say which number you want to test out (100, 1010, 1, 10110, 10000011)\n")
-    value+=".txt"
+    value += ".txt"
     data = open("testfiles/" + value, "r")
     input_string = data.read()
 
     if ("0" in input_string or "1" in input_string):
-        input_string = [int(a) for a in str(input_string)]
+        input_string = [int(a) for a in input_string]
         state_q0(input_string)
     else:
         print("This isn't a binary string.")
